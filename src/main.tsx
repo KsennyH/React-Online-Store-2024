4e409fe2fcaf3e37efe4639f89013501b2bb9bbc
Ksenny <ksenny_h@mail.ru>
2025-10-13 15:19:07 +0300

    Удалена папка с лишними шаблонами

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, RouterProvider} from "react-router-dom";
+import { RouterProvider} from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import './index.css';
