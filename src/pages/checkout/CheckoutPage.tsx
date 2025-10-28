12fb28d94d07f4bdaf40df71bf6b509178a516a7
Ksenny <ksenny_h@mail.ru>
2025-10-28 21:53:35 +0300

    Сделана валидация для формы оформления заказа

--- src/pages/checkout/CheckoutPage.tsx
+++ src/pages/checkout/CheckoutPage.tsx
@@ -1,40 +1,93 @@
-import { JSX, useState } from "react";
+import { JSX } from "react";
 import styles from "./CheckoutPage.module.scss";
 import Title from "@/components/ui/title/Title";
 import Button from "@/components/ui/Button";
 import Checkbox from "@/components/ui/checkbox/Checkbox";
+import { CheckoutSchema } from "./CheckoutSchema";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
 
 function CheckoutPage(): JSX.Element {
-    const [isChecked, setIsChecked] = useState(true);
-    console.log(isChecked);
+    
+    const navigate = useNavigate();
+    const { register, control, handleSubmit, reset, formState: { isDirty, isSubmitting, errors } } = useForm<CheckoutSchema>({ resolver: zodResolver(CheckoutSchema), mode: "onBlur" });
+    const onSubmit: SubmitHandler<CheckoutSchema> = (data) => {
+        console.log(data);
+        toast.success('Форма успешно отправлена', {
+            position: 'top-right',
+            duration: 3000
+        });
+        reset();
+        navigate('/');
+    }
+    
     return(
         <section className={styles.checkout}>
             <div className="container">
                 <div className={styles.checkout__title}>
                     <Title tag="h1">Оформление заказа</Title>
                 </div>
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
                     <label className={styles.form__label}>
                         <span className={styles.form__title}>Имя:</span>
-                        <input className={styles.form__input} type="text" placeholder="Имя" />
+                        <input className={styles.form__input} {...register('name', { required: true })} type="text" placeholder="Имя" />
+                        {errors.name && (
+                            <div style={{ color: '#000' }}>
+                                {errors.name?.message}
+                            </div>
+                        )}
                     </label>
                     <label className={styles.form__label}>
                         <span className={styles.form__title}>E-mail:</span>
-                        <input className={styles.form__input} type="email" placeholder="E-mail" />
+                        <input className={styles.form__input} {...register('email', { required: true })} type="email" placeholder="E-mail" />
+                        {errors.email && (
+                            <div style={{ color: '#000' }}>
+                                {errors.email?.message}
+                            </div>
+                        )}
                     </label>
                     <label className={styles.form__label}>
                         <span className={styles.form__title}>Телефон:</span>
-                        <input className={styles.form__input} type="tel" placeholder="Телефон" />
+                        <input className={styles.form__input} {...register('phone', { required: true })} type="tel" placeholder="Телефон" />
+                        {errors.phone && (
+                            <div style={{ color: '#000' }}>
+                                {errors.phone?.message}
+                            </div>
+                        )}
                     </label>
                     <label className={styles.form__label}>
                         <span className={styles.form__title}>Адрес:</span>
-                        <input className={styles.form__input} type="text" placeholder="Адрес" />
+                        <input className={styles.form__input} {...register('address')} type="text" placeholder="Адрес" />
+                        {errors.address && (
+                            <div style={{ color: '#000' }}>
+                                {errors.address?.message}
+                            </div>
+                        )}
                     </label>
                     <label className={styles.form__label}>
-                        <Checkbox name="Я согласен на обработку персональных данных" checked={isChecked} onChange={(value) => setIsChecked(value)} />
+                        <Controller 
+                            name="check"
+                            control={control}
+                            rules={{ required: true }}
+                            render={({ field }) => (
+                                <Checkbox
+                                    name="Я согласен на обработку персональных данных"
+                                    checked={field.value || false}
+                                    onChange={(checked) => field.onChange(checked)}
+                                />
+                            )}
+                        />
+                        {errors.check && (
+                            <div style={{ color: '#000' }}>
+                                {errors.check?.message}
+                            </div>
+                        )}
                     </label>
                     <div className={styles.form__btn}>
-                        <Button variant="primary" type="submit">Оформить заказ</Button>
+                        <Button variant="primary" type="submit" disabled={!isDirty || isSubmitting}>Оформить заказ</Button>
                     </div>
                 </form>
             </div>
