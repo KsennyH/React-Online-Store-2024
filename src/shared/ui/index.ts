8200fc5bb3a8333f260d90a11f479ed7cea60944
Ksenny <ksenny_h@mail.ru>
2025-11-09 00:38:23 +0300

    FSD:Shared слой

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,5 @@
+export { Button } from './Button/Button';
+export { CloseButton } from './CloseButton/CloseButton';
+export { LinkButton } from './LinkButton/LinkButton';
+export { Title } from './Title/Title';
+export { Checkbox } from './Checkbox/Checkbox';
