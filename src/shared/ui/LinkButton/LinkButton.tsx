534b43200524c0e0c5282c63586735306d6cef45
Ksenny <ksenny_h@mail.ru>
2025-11-09 00:37:01 +0300

    FSD:Shared компонент ссылка

---
+++ src/shared/ui/LinkButton/LinkButton.tsx
@@ -0,0 +1,18 @@
+import cn from "classnames";
+import { LinkButtonProps } from "./LinkButtonProps";
+import styles from './LinkButton.module.scss';
+import { Link } from "react-router-dom";
+import { JSX } from "react";
+export function LinkButton ({ children, href, className, variant='primary', size='lg', isActive, ...rest }: LinkButtonProps): JSX.Element {
+    return (
+        <Link to={href} className={cn(
+                styles.link, 
+                styles[variant], 
+                styles[size],
+                isActive && styles.active,
+                className)
+            } {...rest}>
+            {children}
+        </Link>
+    );
+}
