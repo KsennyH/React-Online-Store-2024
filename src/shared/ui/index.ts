1c2488dca95129c44e52dafbe693778fe229cccf
Ksenny <ksenny_h@mail.ru>
2025-11-09 23:24:26 +0300

    FSD: Shared слой

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,3 +3,6 @@ export { CloseButton } from './CloseButton/CloseButton';
 export { LinkButton } from './LinkButton/LinkButton';
 export { Title } from './Title/Title';
 export { Checkbox } from './Checkbox/Checkbox';
+export { ChangeColorButton } from './ChangeColorButton/ChangeColorButton';
+export { Loader } from './loader/Loader';
+export { ErrorMessage } from './ErrorMessage/ErrorMessage';
