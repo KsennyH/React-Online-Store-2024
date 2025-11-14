8c4b43dd0782860ddf897efc072ccd52319f5442
Ksenny <ksenny_h@mail.ru>
2025-11-14 20:23:20 +0300

    FSD: обновление страницы контактов

--- src/pages/contacts/ui/ContactsPage/ContactsPage.tsx
+++ src/pages/contacts/ui/ContactsPage/ContactsPage.tsx
@@ -1,33 +1,17 @@
 import styles from "./ContactsPage.module.scss";
-import ContactWidget from "@/components/ui/contact-widget/ContactWidget";
-import YandexMap from "@/components/map/YandexMap";
-import { Title } from "@/shared";
+import { Title, YandexMap } from "@/shared/ui";
+import { WidgetsList } from "../WidgetsList/WidgetsList";
 
 export function ContactsPage() {
     return (
         <section className={styles.contacts}>
-            <div className={styles.contacts__title}>
+            <div className={styles.title}>
                 <Title tag="h1">Свяжитесь с нами</Title>
             </div>
-            <div className={styles.contacts__wrapper}>
-                <ContactWidget title="Телефон">
-                    <a href="tel:+74932134581">+7 (4932) 134-581</a>
-                </ContactWidget>
-                <ContactWidget title="Email">
-                    <a href="mailto:motomirIvanovo@email.com">motomir@email.com</a>
-                </ContactWidget>
-                <ContactWidget title="Адрес">
-                    <p>Иваново, Посадский переулок, 8</p>
-                </ContactWidget>
-                <ContactWidget title="Время работы">
-                    <p>Пн–Пт: 9:00 – 19:00</p>
-                    <p>Сб: 9:00 - 17:00</p>
-                </ContactWidget>
+            <WidgetsList />
+            <div className={styles.map}>
+                <YandexMap />
             </div>
-
-      <div className={styles.contacts__map}>
-        <YandexMap />
-      </div>
     </section>
     );
 }
