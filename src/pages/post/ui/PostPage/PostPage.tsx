edeb3bfed060b21152767b2138c27e47cd0a193b
Ksenny <ksenny_h@mail.ru>
2025-11-14 20:23:45 +0300

    FSD: обновление страницы статьи

--- src/pages/post/ui/PostPage/PostPage.tsx
+++ src/pages/post/ui/PostPage/PostPage.tsx
@@ -1,25 +1,10 @@
 import { useParams } from "react-router-dom";
 import styles from "./PostPage.module.scss";
 import { JSX } from "react";
-import PostCategories, { Category } from "@/components/post-category/PostCategories";
-import Tags from "@/components/tags/Tags";
-import Article from "@/components/article/Article";
 import Comments from "@/components/comments/Comments";
-import { useGetSingleNewsQuery } from "@/api/news/newsApi";
-import { Author } from "@/components/post/Post";
-import { ErrorMessage, Loader } from "@/shared";
-import Skeleton from "react-loading-skeleton";
-
-export interface SinglePost {
-    id: number,
-    title: string,
-    image: string,
-    intro: string,
-    content: string,
-    authors: Author,
-    categories?: Category;
-    created_at: string,
-}
+import { ErrorMessage } from "@/shared/ui";
+import { FullArticle } from "@/entities/article";
+import { ArticleAside } from "@/widgets/article";
 
 export function PostPage(): JSX.Element {
 
@@ -29,25 +14,15 @@ export function PostPage(): JSX.Element {
         return <ErrorMessage text="Статья не найдена" />;
     }
 
-    const { data, error } = useGetSingleNewsQuery(id);
-
-    if(error) { 
-        console.error('Ошибка при загрузке новости:', error);
-        return <ErrorMessage text="Не удалось загрузить новость. Попробуйте позже." />        
-    }
-
     return(
         <section className={styles.blogPage}>
             <div className="container">
                 <div className={styles.wrapper}>
                     <div className={styles.content}>
-                        { data && <Article article={ data } /> }
+                        <FullArticle id={id} />
                         <Comments />
                     </div>
-                    <aside className={styles.aside}>
-                        <PostCategories />
-                        <Tags /> 
-                    </aside>
+                    <ArticleAside />
                 </div>
             </div>
         </section>
